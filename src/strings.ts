f16cff6696fe3eecff710e9996082460632783fe
vascob-77 <81617180+vascob-77@users.noreply.github.com>
2023-05-26 09:15:25 +0200

    Update strings.ts

--- src/strings.ts
+++ src/strings.ts
@@ -16,34 +16,34 @@
 export class Html5QrcodeStrings {
 
     public static codeParseError(exception: any): string {
-        return `QR code parse error, error = ${exception}`;
+        return `Erreur d'analyse du code QR, erreur = ${exception}`;
     }
 
     public static errorGettingUserMedia(error: any): string {
-        return `Error getting userMedia, error = ${error}`;
+        return `Erreur lors de l'obtention de userMedia, erreur = ${error}`;
     }
 
     public static onlyDeviceSupportedError(): string {
-        return "The device doesn't support navigator.mediaDevices , only "
-        + "supported cameraIdOrConfig in this case is deviceId parameter "
-        + "(string).";
+        return "Le dispositif ne prend pas en charge navigator.mediaDevices, "
+        + "seul l'identifiant de caméra ou la configuration de l'ID de dispositif "
+        + "sont pris en charge dans ce cas (en tant que chaîne de caractères).";
     }
 
     public static cameraStreamingNotSupported(): string {
-        return "Camera streaming not supported by the browser.";
+        return "La diffusion en continu de la caméra n'est pas prise en charge par le navigateur.";
     }
 
     public static unableToQuerySupportedDevices(): string {
-        return "Unable to query supported devices, unknown error.";
+        return "Impossible d'interroger les dispositifs pris en charge, erreur inconnue.";
     }
 
     public static insecureContextCameraQueryError(): string {
-        return "Camera access is only supported in secure context like https "
-        + "or localhost.";
+        return "L'accès à la caméra est uniquement pris en charge dans un contexte sécurisé "
+        + "tel que https ou localhost.";
     }
 
     public static scannerPaused(): string {
-        return "Scanner paused";
+        return "Scanner en pause";
     }
 }
 
@@ -55,71 +55,71 @@ export class Html5QrcodeStrings {
 export class Html5QrcodeScannerStrings {
 
     public static scanningStatus(): string {
-        return "Scanning";
+        return "Analyse en cours";
     }
 
     public static idleStatus(): string {
-        return "Idle";
+        return "Inactif";
     }
 
     public static errorStatus(): string {
-        return "Error";
+        return "Erreur";
     }
 
     public static permissionStatus(): string {
-        return "Permission";
+        return "Autorisation";
     }
 
     public static noCameraFoundErrorStatus(): string {
-        return "No Cameras";
+        return "Aucune caméra";
     }
 
     public static lastMatch(decodedText: string): string {
-        return `Last Match: ${decodedText}`;
+        return `Dernière correspondance : ${decodedText}`;
     }
 
     public static codeScannerTitle(): string {
-        return "Code Scanner";
+        return "Scanner de codes";
     }
 
     public static cameraPermissionTitle(): string {
-        return "Request Camera Permissions";
+        return "Demande d'autorisation de la caméra";
     }
 
     public static cameraPermissionRequesting(): string {
-        return "Requesting camera permissions...";
+        return "Demande d'autorisation de la caméra en cours...";
     }
 
     public static noCameraFound(): string {
-        return "No camera found";
+        return "Aucune caméra trouvée";
     }
 
     public static scanButtonStopScanningText(): string {
-        return "Stop Scanning";
+        return "Arrêter l'analyse";
     }
 
     public static scanButtonStartScanningText(): string {
-        return "Start Scanning";
+        return "Démarrer l'analyse";
     }
 
     public static torchOnButton(): string {
-        return "Switch On Torch";
+        return "Activer la torche";
     }
 
     public static torchOffButton(): string {
-        return "Switch Off Torch";
+        return "Désactiver la torche";
     }
 
     public static torchOnFailedMessage(): string {
-        return "Failed to turn on torch";
+        return "Impossible d'activer la torche";
     }
 
     public static torchOffFailedMessage(): string {
-        return "Failed to turn off torch";
+        return "Impossible de désactiver la torche";
     }
 
     public static scanButtonScanningStarting(): string {
-        return "Launching Camera...";
+        return "Démarrage de la caméra...";
     }
 
     /**
@@ -128,7 +128,7 @@ export class Html5QrcodeScannerStrings {
      * This will be used to switch to file based scanning.
      */
     public static textIfCameraScanSelected(): string {
-        return "Scan an Image File";
+        return "Scanner un fichier image";
     }
 
     /**
@@ -137,36 +137,36 @@ export class Html5QrcodeScannerStrings {
      * This will be used to switch to camera based scanning.
      */
     public static textIfFileScanSelected(): string {
-        return "Scan using camera directly";
+        return "Scanner directement avec la caméra";
     }
 
     public static selectCamera(): string {
-        return "Select Camera";
+        return "Sélectionner une caméra";
     }
 
     public static fileSelectionChooseImage(): string {
-        return "Choose Image";
+        return "Choisir une image";
     }
 
     public static fileSelectionChooseAnother(): string {
-        return "Choose Another";
+        return "Choisir une autre";
     }
 
     public static fileSelectionNoImageSelected(): string {
-        return "No image choosen";
+        return "Aucune image sélectionnée";
     }
 
     /** Prefix to be given to anonymous cameras. */
     public static anonymousCameraPrefix(): string {
-        return "Anonymous Camera";
+        return "Caméra anonyme";
     }
 
     public static dragAndDropMessage(): string {
-        return "Or drop an image to scan";
+        return "Ou glissez-déposez une image à scanner";
     }
 
     public static dragAndDropMessageOnlyImages(): string {
-        return "Or drop an image to scan (other files not supported)";
+        return "Ou glissez-déposez une image à scanner (les autres fichiers ne sont pas pris en charge)";
     }
 
     /** Value for zoom. */
@@ -175,15 +175,15 @@ export class Html5QrcodeScannerStrings {
     }
 
     public static loadingImage(): string {
-        return "Loading image...";
+        return "Chargement de l'image...";
     }
 
     public static cameraScanAltText(): string {
-        return "Camera based scan";
+        return "Scan basé sur la caméra";
     }
 
     public static fileScanAltText(): string {
-        return "Fule based scan";
+        return "Scan basé sur un fichier";
     }
 }
 
@@ -191,10 +191,10 @@ export class Html5QrcodeScannerStrings {
 export class LibraryInfoStrings {
 
     public static poweredBy(): string {
-        return "Powered by ";
+        return "Propulsé par ";
     }
 
     public static reportIssues(): string {
-        return "Report issues";
+        return "Signaler des problèmes";
     }
 }
